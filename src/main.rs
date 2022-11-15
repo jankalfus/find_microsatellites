9e6410e6143b492deb87b20a9af9f7c7e12cbfd9
Jan Kalfus <jankalfus@gmail.com>
2022-11-15 18:47:11 +0100

    Add tests for count_matches

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,51 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     Ok(io::BufReader::new(file).lines())
 }
 
+#[cfg(test)]
+mod count_matches_tests {
+    use crate::count_matches;
+
+    #[test]
+    fn empty_string() {
+        assert_eq!(count_matches("", "AA"), vec![]);
+    }
+
+    #[test]
+    fn no_match() {
+        assert_eq!(count_matches("TATT", "AA"), vec![])
+    }
+
+    #[test]
+    fn single_match() {
+        assert_eq!(count_matches("AA", "AA"), vec![1]);
+    }
+
+    #[test]
+    fn single_match_in_odd_number_of_chars() {
+        assert_eq!(count_matches("AAA", "AA"), vec![1]);
+    }
+
+    #[test]
+    fn multiple_matches() {
+        assert_eq!(count_matches("AAAA", "AA"), vec![2]);
+    }
+
+    #[test]
+    fn multiple_distant_matches() {
+        assert_eq!(count_matches("AATTAA", "AA"), vec![1, 1]);
+    }
+
+    #[test]
+    fn multiple_various_matches() {
+        assert_eq!(count_matches("AAAAAATTAAGAA", "AA"), vec![3, 1, 1]);
+    }
+
+    #[test]
+    fn odd_pattern() {
+        assert_eq!(count_matches("TATTATGATGATTAT", "GAT"), vec![2]);
+    }
+}
+
 fn count_matches(str: &str, pattern: &str) -> Vec<usize> {
     let mut counts = Vec::new();
 
